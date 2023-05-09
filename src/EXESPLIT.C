52ef77cb96599a016fb61c8215773900a07881cc
h0ly0ne <32588743+h0ly0ne@users.noreply.github.com>
2023-05-10 01:04:13 +0200

    * Added MERGE mode for EXESPLIT for debugging purposes * Minor typo fixes in IO assembler routines

--- src/EXESPLIT.C
+++ src/EXESPLIT.C
@@ -1,6 +1,8 @@
 /*
  * Borrowed from Extended Operating System Loader
  * Copyright (c) 1999 by Geurt Vos
+ * Adopted and extended for Ranish Partition Manager
+ * Copyright (c) 2023 by Oliver Oswald
  *
  * This code is distributed under GNU General Public License (GPL)
  *
@@ -11,23 +13,44 @@
 #include <exehead.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void Usage(void);
 void DoExesplit(char *, char *);
+void DoMerge(char *, char *);
 void WriteHeader(int iInputFileHandle, char *);
 void WriteData(int iInputFileHandle, char *);
 
-static char baBuffer[32768];
+static char *baBuffer;
 
 void main(int argc, char **argv) {
-	if( argc==3 ) DoExesplit(argv[1],argv[2]);
- 	else Usage();
+    baBuffer = (char *)malloc(sizeof(char));
+
+	if(argc == 4) {
+        if(strcmpi(argv[1], "split")==0) {
+            DoExesplit(argv[2], argv[3]);
+        }
+        else if(strcmpi(argv[1], "merge")==0) {
+            DoMerge(argv[2], argv[3]);
+        }
+        else {
+            Usage();
+        }
+    } 
+ 	else {
+        Usage();
+    }
 
 	exit(0);
 }
 
 void Usage(void) {
- 	fprintf(stderr,	"Usage: exesplit part.exe xrpart00.xxf\n");
+ 	fprintf(stderr,	"Usage: EXESPLIT.EXE SPLIT <SOURCEFILE> <TARGETFILE(S)>\n");
+    fprintf(stderr,	"       EXESPLIT.EXE MERGE <SOURCEFILE(S)> <TARGETFILE>\n");
+    fprintf(stderr,	"\n");
+    fprintf(stderr,	"Example: EXESPLIT.EXE SPLIT PART.EXE XRPART00.XXF\n");
+    fprintf(stderr,	"         EXESPLIT.EXE MERGE XRPART00.XXF PART.EXE\n");
+
 	exit(1);
 }
 
@@ -39,30 +62,91 @@ void DoExesplit(char *strSourceFilename, char *strTargetFilename) {
 	close(iInputFileHandle);
 }
 
+void DoMerge(char *strSourceFilename, char *strTargetFileName) {
+    exehead_dos_sixteenbit ehdstbInputFileHeader;
+    unsigned short ulInputFileParts;
+    unsigned short ulInputLastPartSize;
+    int iInputFileHandle;
+    int iOutputFileHandle = create(strTargetFileName);
+    unsigned long ulInputFileSize = 0;
+    unsigned long ulOutputFileSize = 0;
+    unsigned int uInputFileIndex = 0;
+
+    iInputFileHandle = open(strSourceFilename, O_RDONLY);
+    ulInputFileSize = (unsigned long)getSize(iInputFileHandle);
+    read(iInputFileHandle, &ehdstbInputFileHeader, (unsigned int)sizeof(ehdstbInputFileHeader));
+    ulInputLastPartSize = (unsigned short)ehdstbInputFileHeader.e_res2[8];
+	ulInputFileParts = (unsigned short)ehdstbInputFileHeader.e_res2[9];
+    ehdstbInputFileHeader.e_res2[8] = 0;
+    ehdstbInputFileHeader.e_res2[9] = 0;
+    write(iOutputFileHandle, &ehdstbInputFileHeader, (unsigned int)sizeof(ehdstbInputFileHeader));
+
+    baBuffer = (char *)realloc(baBuffer, (unsigned long)(sizeof(char)*(ehdstbInputFileHeader.e_cparhdr*16-(unsigned int)sizeof(ehdstbInputFileHeader))));
+    ulOutputFileSize += ulInputFileSize;
+    read(iInputFileHandle, baBuffer, (ehdstbInputFileHeader.e_cparhdr*16-(unsigned int)sizeof(ehdstbInputFileHeader)));
+    close(iInputFileHandle);
+	write(iOutputFileHandle, baBuffer, (ehdstbInputFileHeader.e_cparhdr*16-(unsigned int)sizeof(ehdstbInputFileHeader)));
+
+    ++strSourceFilename[7];
+
+    for(uInputFileIndex = 1; uInputFileIndex <= ulInputFileParts; uInputFileIndex++) {
+        if(uInputFileIndex < ulInputFileParts) {
+            iInputFileHandle = open(strSourceFilename, O_RDONLY);
+            ulInputFileSize = (unsigned long)getSize(iInputFileHandle);
+            baBuffer = (char *)realloc(baBuffer, (unsigned long)(sizeof(char)*ulInputFileSize));
+            ulOutputFileSize += ulInputFileSize;
+
+            if(ulInputFileSize == 32768) {
+                read(iInputFileHandle, baBuffer, ulInputFileSize);
+                close(iInputFileHandle);
+                write(iOutputFileHandle, baBuffer, ulInputFileSize);
+            }
+            else {
+                exit(1);
+            }
+        }
+        else {
+            iInputFileHandle = open(strSourceFilename, O_RDONLY);
+            ulInputFileSize = (unsigned long)getSize(iInputFileHandle);
+            baBuffer = (char *)realloc(baBuffer, (unsigned long)(sizeof(char)*ulInputFileSize));
+            ulOutputFileSize += ulInputFileSize;
+
+            if(ulInputFileSize == ulInputLastPartSize) {
+                read(iInputFileHandle, baBuffer, ulInputFileSize);
+                close(iInputFileHandle);
+                write(iOutputFileHandle, baBuffer, ulInputFileSize);
+            }
+            else {
+                exit(1);
+            }
+        }
+
+        ++strSourceFilename[7];
+	}
+
+    close(iOutputFileHandle);
+}
+
 void WriteHeader(int iInputFileHandle, char *strTargetFilename) {
 	exehead_dos_sixteenbit ehdstbInputFileHeader;
 	unsigned long ulOutputFileParts;
-	unsigned long ulOutputFileSizeWithoutHeader;
-	unsigned long ulOutputRestHeaderSize;
 
 	int iOutputFileHandle = create(strTargetFilename);
-	unsigned uInputFileHeaderSize = sizeof(ehdstbInputFileHeader);
 	unsigned long ulInputFileSize = getSize(iInputFileHandle);
 	
-	read(iInputFileHandle, &ehdstbInputFileHeader, uInputFileHeaderSize);
+	read(iInputFileHandle, &ehdstbInputFileHeader, sizeof(ehdstbInputFileHeader));
 
-	ulOutputRestHeaderSize = ehdstbInputFileHeader.e_cparhdr * 16 - uInputFileHeaderSize;
-	ulOutputFileSizeWithoutHeader = (unsigned long)((unsigned long)ulInputFileSize - ((unsigned long)ehdstbInputFileHeader.e_cparhdr * 16));
 	ulOutputFileParts = ulInputFileSize;
-	ulOutputFileParts -= (unsigned long)(ehdstbInputFileHeader.e_cparhdr * 16);
-	ulOutputFileParts = 1 + ulOutputFileParts / 32768 + !!(ulOutputFileParts % 32768);
+	ulOutputFileParts -= (unsigned long)(ehdstbInputFileHeader.e_cparhdr*16);
+	ulOutputFileParts = 1+ulOutputFileParts/32768+!!(ulOutputFileParts%32768);
 	ulOutputFileParts--;
-	ehdstbInputFileHeader.e_res2[8] = (unsigned short)(ulOutputFileSizeWithoutHeader - ((ulOutputFileParts - 1) * 32768));
+	ehdstbInputFileHeader.e_res2[8] = (unsigned short)(((unsigned long)((unsigned long)ulInputFileSize-((unsigned long)ehdstbInputFileHeader.e_cparhdr*16)))-((ulOutputFileParts-1)*32768));
 	ehdstbInputFileHeader.e_res2[9] = (unsigned short)ulOutputFileParts;
+    baBuffer = (char *)realloc(baBuffer, (unsigned long)(sizeof(char)*ulInputFileSize));
 
-	write(iOutputFileHandle,&ehdstbInputFileHeader,uInputFileHeaderSize);
-	read(iInputFileHandle,baBuffer,ulOutputRestHeaderSize);
-	write(iOutputFileHandle,baBuffer,ulOutputRestHeaderSize);
+	write(iOutputFileHandle, &ehdstbInputFileHeader, sizeof(ehdstbInputFileHeader));
+	read(iInputFileHandle, baBuffer, ehdstbInputFileHeader.e_cparhdr*16-sizeof(ehdstbInputFileHeader));
+	write(iOutputFileHandle, baBuffer, ehdstbInputFileHeader.e_cparhdr*16-sizeof(ehdstbInputFileHeader));
 	close(iOutputFileHandle);
 	++strTargetFilename[7];
 }
@@ -71,9 +155,10 @@ void WriteData(int iInputFileHandle, char *strTargetFilename) {
 	unsigned short Bytes;
 	int iOutputFileHandle;
 
-	while ((Bytes = read(iInputFileHandle,baBuffer,32768)) != 0) {
+    baBuffer = (char *)realloc(baBuffer, (unsigned long)(sizeof(char)*32768));
+	while((Bytes = read(iInputFileHandle, baBuffer, 32768)) != 0) {
 		iOutputFileHandle = create(strTargetFilename);
-		write(iOutputFileHandle,baBuffer,Bytes);
+		write(iOutputFileHandle, baBuffer, Bytes);
 		close(iOutputFileHandle);
 		++strTargetFilename[7];
 	}
